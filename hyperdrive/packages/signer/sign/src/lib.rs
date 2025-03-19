c065388db53cf6ebe6bb74cb83b17f8d1eb7311c
polwex <polwex@sortug.com>
2025-03-19 22:38:39 +0700

    refactored and addressed PR comments

--- hyperdrive/packages/signer/sign/src/lib.rs
+++ hyperdrive/packages/signer/sign/src/lib.rs
@@ -1,7 +1,8 @@
+use crate::hyperware::process::sign;
 use anyhow::{anyhow, Result};
+use hyperware_process_lib::logging::{error, init_logging, Level};
 use hyperware_process_lib::net::{NetAction, NetResponse};
 use hyperware_process_lib::{await_message, call_init, Address, Message, Request, Response};
-use serde::{Deserialize, Serialize};
 
 wit_bindgen::generate!({
     path: "target/wit",
@@ -10,76 +11,66 @@ wit_bindgen::generate!({
     additional_derives: [PartialEq, serde::Deserialize, serde::Serialize, process_macros::SerdeJsonInto],
 });
 
-#[derive(Debug, Serialize, Deserialize)]
-enum SignerRequest {
-    Sign(SignRequest),
-    Verify { from: Address, data: SignResponse },
-}
-#[derive(Debug, Serialize, Deserialize)]
-struct SignRequest {
-    pub site: String,
-    pub time: u64,
-    pub nonce: Option<String>,
-}
-#[derive(Debug, Serialize, Deserialize)]
-struct SignResponse {
-    pub body: SignRequest,
-    pub message: Vec<u8>,
-    pub signature: Vec<u8>,
-}
-
 call_init!(initialize);
 fn initialize(our: Address) {
+    // this seems to require calling vfs:distro:sys
+    init_logging(Level::DEBUG, Level::INFO, None, None, None).unwrap();
     loop {
-        let msg = await_message();
-        match msg {
-            Err(_send_error) => {
-                // ignore send errors, local-only process
-                continue;
+        match await_message() {
+            Err(send_error) => error!("got SendError: {send_error}"),
+            Ok(ref message) => {
+                match message {
+                    Message::Request { body, .. } => match handle_request(&our, &body) {
+                        Ok(_) => continue,
+                        Err(request_error) => {
+                            error!("error handling sign request: \n{request_error}")
+                        }
+                    },
+                    _ => error!("Response received at sign process"), // we are awaiting all requests no resposes should be received
+                }
             }
-            Ok(Message::Request { body, .. }) => handle_request(&our, &body).unwrap_or_default(),
-            _ => continue, // ignore responses
         }
     }
 }
 
 fn handle_request(our: &Address, request_bytes: &[u8]) -> Result<()> {
-    let req = serde_json::from_slice::<SignerRequest>(request_bytes)?;
-    match req {
-        SignerRequest::Sign(r) => handle_sign(our, r, request_bytes),
-        SignerRequest::Verify { from, data } => handle_verify(from, data),
+    match request_bytes.try_into()? {
+        sign::Request::Sign(bytes) => handle_sign(bytes),
+        sign::Request::Verify(req) => handle_verify(our, req),
     }
 }
 
-fn handle_sign(our: &Address, req: SignRequest, request_bytes: &[u8]) -> Result<()> {
+fn handle_sign(bytes: Vec<u8>) -> Result<()> {
     let body = rmp_serde::to_vec(&NetAction::Sign)?;
     let res = Request::to(("our", "net", "distro", "sys"))
-        .blob_bytes(request_bytes)
+        .blob_bytes(bytes.clone())
         .body(body)
         .send_and_await_response(10)??;
     let Ok(NetResponse::Signed) = rmp_serde::from_slice::<NetResponse>(res.body()) else {
         return Err(anyhow!("signature failed"));
     };
-    let newblob = res.blob();
-    let message = [our.to_string().as_bytes(), request_bytes].concat();
-    match newblob {
+    let signature = res.blob();
+    match signature {
         None => Err(anyhow!("no blob")),
         Some(b) => {
-            let lr = SignResponse {
-                body: req,
-                message,
+            let sign_response = sign::SignResponse {
+                message: bytes,
                 signature: b.bytes().to_vec(),
             };
-            let lrj = serde_json::to_vec(&lr)?;
-            Response::new().body(lrj).send()?;
+            let sign_response_bytes = serde_json::to_vec(&sign_response)?;
+            Response::new().body(sign_response_bytes).send()?;
             Ok(())
         }
     }
 }
-fn handle_verify(from: Address, data: SignResponse) -> Result<()> {
-    let signature = data.signature;
-    let body = rmp_serde::to_vec(&NetAction::Verify { from, signature })?;
-    let req_bytes = rmp_serde::to_vec(&data.body)?;
+fn handle_verify(our: &Address, req: sign::VerifyRequest) -> Result<()> {
+    let process = our.to_owned().process;
+    let from = Address::new(req.node, process);
+    let body = rmp_serde::to_vec(&NetAction::Verify {
+        from,
+        signature: req.signature,
+    })?;
+    let req_bytes = rmp_serde::to_vec(&body)?;
     let res = Request::to(("our", "net", "distro", "sys"))
         .blob_bytes(req_bytes)
         .body(body)
