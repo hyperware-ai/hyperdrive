71aeffa5cbe124345fe315645c0ad9a74d9e0ec5
hosted-fornet <hosted-fornet@protonmail.com>
2025-06-17 07:18:41 -0700

    add mobile-first homepage

--- hyperdrive/packages/homepage/ui/src/main.tsx
+++ hyperdrive/packages/homepage/ui/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Homepage from './pages/Homepage.tsx'
+//import Homepage from './pages/Homepage.tsx'
+import AndroidHomescreen from './pages/AndroidHomescreen.tsx'
 import './index.css'
 import 'uno.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Homepage />
+    <AndroidHomescreen />
   </React.StrictMode>,
 )
